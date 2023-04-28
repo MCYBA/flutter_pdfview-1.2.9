bfdec45d68a0c19f1f17b9739e5565d85decf479
baybars <cem.baybars@gmail.com>
2023-04-28 17:01:56 +0300

    First commit

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/DepoLIN/home/baybars/flutter"
+export "FLUTTER_APPLICATION_PATH=/DepoLIN/home/baybars/flutter_pdfview-1.2.9/example"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1.0.0"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
